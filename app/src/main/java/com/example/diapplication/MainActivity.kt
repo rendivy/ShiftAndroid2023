8d57d6e89969b1d86dab1583e33c7d5c07dea471
rendivy <arlosaneq@gmail.com>
2023-07-06 00:03:44 +0700

    Исправил проблему с CleartextTraffic и вынес логику получения погоды во вью модель

--- app/src/main/java/com/example/diapplication/MainActivity.kt
+++ app/src/main/java/com/example/diapplication/MainActivity.kt
@@ -11,13 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diapplication.ui.theme.DIapplicationTheme
+import com.example.diapplication.view.WeatherScreenPreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DIapplicationTheme {
-
+                WeatherScreenPreview()
             }
         }
     }
--- app/src/main/java/com/example/diapplication/view/WeatherScreen.kt
+++ app/src/main/java/com/example/diapplication/view/WeatherScreen.kt
@@ -1,7 +1,6 @@
 package com.example.diapplication.view
 
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,25 +10,23 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.TopCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.TopCenter
 import com.example.diapplication.ui.theme.lightWeatherColor
 import com.example.diapplication.ui.theme.weatherButtonText
 import com.example.diapplication.viewModel.WeatherViewModel
-import kotlinx.coroutines.runBlocking
 
 
 @Preview(showBackground = true)
 @Composable
 fun WeatherScreenPreview() {
-    val weatherViewModel: WeatherViewModel = WeatherViewModel()
+    val weatherViewModel = WeatherViewModel()
     WeatherScreen(weatherViewModel = weatherViewModel)
 }
 
@@ -37,7 +34,7 @@ fun WeatherScreenPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun WeatherScreen(weatherViewModel: WeatherViewModel) {
-    var weatherCast by remember { mutableStateOf("--") }
+    val weatherCast = remember { mutableStateOf("--") }
     val cityName = remember { mutableStateOf("") }
     Box(
         modifier = Modifier
@@ -45,7 +42,7 @@ private fun WeatherScreen(weatherViewModel: WeatherViewModel) {
             .background(lightWeatherColor)
     ) {
         Text(
-            text = weatherCast, fontSize = 100.sp,
+            text = weatherCast.value, fontSize = 100.sp,
             modifier = Modifier
                 .align(Alignment.Center)
                 .padding(top = 32.dp),
@@ -58,19 +55,13 @@ private fun WeatherScreen(weatherViewModel: WeatherViewModel) {
             label = { Text("Enter city name") }
         )
 
-        /*Не очень понимаю, куда стоит вынести данный функционал кнопки,
-        т.к. он не относится к UI, а кнопка в целом должна быть глупенькая и просто вызывать какую-то функцию
-        */
         Button(
-                onClick = {
-
-                    runBlocking { weatherViewModel.getWeather(cityName.value) }
-                    weatherCast =
-                        weatherViewModel.weather?.current?.temperatureCelsius.toString() + "°C"
-                },
-                modifier = Modifier.align(Alignment.BottomCenter)
-            ) {
-                Text(text = weatherButtonText)
-            }
+            onClick = {
+                weatherViewModel.updateWeatherData(weatherViewModel, weatherCast, cityName)
+            },
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            Text(text = weatherButtonText)
         }
     }
+}
