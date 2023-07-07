1e3ee6f2157f717a5b79051c0d1cf7531b2208db
rendivy <arlosaneq@gmail.com>
2023-07-07 22:19:22 +0700

    Implement DI with dagger-hilt

--- app/src/main/java/com/example/diapplication/MainActivity.kt
+++ app/src/main/java/com/example/diapplication/MainActivity.kt
@@ -3,22 +3,21 @@ package com.example.diapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
 import com.example.diapplication.ui.theme.DIapplicationTheme
-import com.example.diapplication.view.WeatherScreenPreview
+import com.example.diapplication.view.WeatherScreen
+import com.example.diapplication.viewModel.WeatherViewModel
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: WeatherViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DIapplicationTheme {
-                WeatherScreenPreview()
+                WeatherScreen(viewModel)
             }
         }
     }
